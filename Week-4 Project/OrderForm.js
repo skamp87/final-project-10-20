9d1b11be2877786bd7d3cf5a93ebaa4d875982ef
Susan Kampen <skampen@live.com>
2020-11-17 22:22:42 -0500

    Continuing working on final project

--- Week-4 Project/OrderForm.js
+++ Week-4 Project/OrderForm.js
@@ -1,25 +1,46 @@
-// Get the modal
+// popup
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// button opening popup
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+// <span> closing popup
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// button clicks button, popup opens
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// clicks on <span> (x), close popup
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// click outside of popup, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+// slides JS
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
 }
